73a5fec3370436367a5c20b75ffd88cd45ada26a
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-09-06 11:24:50 -0500

    Changed the tutorials page

---
+++ src/components/TutorialText.js
@@ -0,0 +1,10 @@
+//import React from "React"
+import React from "react";
+
+const TutorialText = () => (
+  <div>
+<h1> Tutorial Quetzal Volador</h1>
+  <p class="w3-center"> Hola buen dia Code Monkeys hoy vamos a enseñarles a hacer el famoso juego de flappy CAT .El objetivo de flappy bird es llegar lo más lejos posible sin chocar con los obstáculos. Así que les enseñaremos a usar la lógica para poder crear este juego divertido en la plataforma creada por MIT (Massachusetts institute of Technology). </p>
+</div>
+);
+export default TutorialText

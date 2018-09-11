824a9a1a36da498b5feb02080618078f8c89638c
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-09-11 10:54:34 -0500

    temporary fix for registration request errors

--- src/components/SubmitTutorial.js
+++ src/components/SubmitTutorial.js
@@ -2,34 +2,6 @@ import React, { Component } from 'react';
 import {BrowserRouter, Route } from "react-router-dom";
 import Footer from "./footer"
 
-handleSubmit(event) {
-    event.preventDefault()
-    var data = {
-        name: this.state.name,
-        description: this.state.description,
-        additional_desc: this.state.additional_desc
-    }
-    console.log(data)
-
-    fetch("/Tutorial/new", {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    }).then(function(response) {
-        if (response.status >= 400) {
-          throw new Error("Bad response from server");
-        }
-        return response.json();
-    }).then(function(data) {
-        console.log(data)
-        if(data == "success"){
-           this.setState({msg: "Thanks for registering"});
-        }
-    }).catch(function(err) {
-        console.log(err)
-    });
-}
-
 const Contact = () => {
   return (
     <div>

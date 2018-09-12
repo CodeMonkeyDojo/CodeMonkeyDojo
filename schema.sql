42308728971beaa9048fe1658ca25e56708aca68
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-09-12 10:14:50 -0500

    finish tutorial schema, and add dummy data

--- schema.sql
+++ schema.sql
@@ -74,9 +74,15 @@ INSERT INTO Forum
 CREATE TABLE Tutorial (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(36) NOT NULL,
+    video_id varchar (36) NOT NULL,
     description varchar(200) NOT NULL,
     additional_desc varchar(200) NOT NULL,
     parent_id int NOT NULL,
     datetime datetime NOT NULL,
     PRIMARY KEY (ID)
   );
+
+INSERT INTO Tutorial
+  (name, video_id, description, additional_desc)
+  VALUES
+  ("Flappy Bird", "ex20943id", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",  )
